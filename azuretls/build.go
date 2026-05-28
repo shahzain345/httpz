fb88e931c9be854b51b3e08e7baf40d3f71415e5
Malik Shahzain <71645463+shahzain345@users.noreply.github.com>
2026-05-28 20:04:36 +0500

    delete

--- azuretls/build.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "C"
-
-// Build with:
-// go build -buildmode=c-shared -o libazuretls.so client.go
-// This will generate libazuretls.so and libazuretls.h
-
-// Include exported functions from client.go
